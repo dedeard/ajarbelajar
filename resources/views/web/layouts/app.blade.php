64223ed514a41a0a7e4c61387e15a2199a364e3a
dedeariansya <dedeariansya1@gmail.com>
2020-03-13 03:44:28 +0800

    update

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -95,6 +95,15 @@
     @include('web.layouts.components.footer')
   </div>
   <script src="{{ asset('js/app.js') }}"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-160467804-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-160467804-1');
+  </script>
   @yield('script')
 </body>
 
