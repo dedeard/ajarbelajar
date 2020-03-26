40c3a7592f468d0736a990234f2647a4dc4efc09
dedeariansya <dedeariansya1@gmail.com>
2020-03-26 19:03:04 +0800

    scheduling delete expired notification

---
+++ app/Console/Commands/DeleteExpiredNotifications.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class DeleteExpiredNotifications extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notification:clean-expired';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deleting expired user notifications.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        DB::table('notifications')->where('created_at', '<', Carbon::now()->subDays(30)->format('Y-m-d'))->delete();
+    }
+}
