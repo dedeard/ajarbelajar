3552fd096a13a2e8e940bc1a6ab523a046d86883
dedeariansya <dedeariansya1@gmail.com>
2020-03-29 22:21:52 +0800

    update

--- resources/views/web/dashboard/components/layoutWrapper.blade.php
+++ resources/views/web/dashboard/components/layoutWrapper.blade.php
@@ -20,7 +20,7 @@ window.SIDEBAR_CLOSE = true
   </div>
 </div>
 <div class="container-fluid">
-  @if(Auth::user()->email_verified_at)
+  @if(Auth::user()->email_verified_at === null)
   <div class="alert alert-warning text-center">
     Alamat Email Anda belum di Verifikasi. silahkan cek email anda kemudian klik link Verifikasinya, <br>
     Jika anda belum menerima email Verifikasinya silahkan klik <a href="{{ route('verification.notice') }}" class="">Disini</a>
