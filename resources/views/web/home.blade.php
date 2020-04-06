64f310d0c2deb5666be65611214a311172c4711c
dedeariansya <dedeariansya1@gmail.com>
2020-04-06 17:55:30 +0800

    Update

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -2,7 +2,12 @@
 @section('content')
 <div class="container-fluid mt-15">
     @include('web.partials.popular-video-lg')
+    <div class="my-20"></div>
     @include('web.partials.new-videos')
+    <div class="my-20"></div>
     @include('web.partials.popular-categories')
+    <div class="my-20"></div>
+    @include('web.partials.most-minitutor-point')
+    <div class="my-20"></div>
 </div>
 @endsection
---
+++ resources/views/web/partials/most-minitutor-point.blade.php
@@ -0,0 +1,21 @@
+<div class="most-minitutor-point">
+  <div class="most-minitutor-point-header">
+    <h3 class="most-minitutor-point-title">MiniTutor dengan Poin terbanyak</h3>
+    <div class="most-minitutor-point-links">
+      <!-- <a href="#" class="btn btn-primary btn-sm">Poin Minitutor?</a> -->
+    </div>
+  </div>
+  <div class="row">
+    @foreach(\App\Model\Minitutor::orderBy('points', 'desc')->limit(4)->get() as $minitutor)
+    <div class="col-lg-3">
+      <a class="most-minitutor-point-card" href="{{ route('minitutor.show', $minitutor->user->username) }}">
+        <div class="avatar">
+          <img class="avatar-holder" src="{{ $minitutor->user->imageUrl() }}" alt="{{ $minitutor->user->username }}">
+        </div>
+        <h4 class="info-name text-truncate">{{ $minitutor->user->name() }}</h4>
+        <span class="info-point">{{ $minitutor->points }} Poin</span>
+      </a>
+    </div>
+    @endforeach
+  </div>
+</div>
