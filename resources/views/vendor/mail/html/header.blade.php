e610a3e39215cced0832d0377ecf6f4ca2648590
dedeariansya <dedeariansya1@gmail.com>
2020-04-11 21:09:57 +0800

    update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
   <td class="header">
       <a href="{{ $url }}" class="logo-img">
-        <img height="40px" alt="Logo ajarbelajar.com" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNjQuNjcgMTY0LjY3Ij48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6IzY3N2FlNDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPmxvZ28gYWphcmJlbGFqYXIuY29tPC90aXRsZT48ZyBpZD0iTGF5ZXJfMiIgZGF0YS1uYW1lPSJMYXllciAyIj48ZyBpZD0iTGF5ZXJfMS0yIiBkYXRhLW5hbWU9IkxheWVyIDEiPjxnIGlkPSJMYXllcl8yLTIiIGRhdGEtbmFtZT0iTGF5ZXIgMiI+PGcgaWQ9IkxheWVyXzEtMi0yIiBkYXRhLW5hbWU9IkxheWVyIDEtMiI+PHBhdGggY2xhc3M9ImNscy0xIiBkPSJNMTY0LjY3LDgyLjM0QTgyLjMyLDgyLjMyLDAsMCwwLDQwLjgsMTEuMjYsMjEuNzUsMjEuNzUsMCwwLDAsMCwyMS43NlY4Mi4zNEgwYTgyLjMyLDgyLjMyLDAsMCwwLDEyMy44Nyw3MS4wOCwyMS43NCwyMS43NCwwLDAsMCwxOSwxMS4yNWgwYTIxLjc1LDIxLjc1LDAsMCwwLDIxLjc1LTIxLjc1VjgyLjM0Wk04Mi4zMywxMjEuMThhMzguODMsMzguODMsMCwxLDEsMzguODMtMzguODNoMEEzOC44MywzOC44MywwLDAsMSw4Mi4zMywxMjEuMThaIi8+PC9nPjwvZz48L2c+PC9nPjwvc3ZnPg==" />
+        <img height="40px" alt="Logo ajarbelajar.com" src="{{secure_asset('img/logo/logo.svg')}}" />
       </a>
       <br>
       <a href="{{ $url }}" class="logo-text">
