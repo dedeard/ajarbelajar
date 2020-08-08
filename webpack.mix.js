d2240df93aac1ff4b27d3561f89de3ec7eaf9fe2
Dede Ariansya <dedeariansya1@gmail.com>
2020-08-08 10:24:41 +0800

    install laravel mix manifest

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,33 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-mix.webpackConfig({ resolve: { alias: { 'vars': path.resolve('resources/sass/theme/_vars.scss') } } })
-   .js('resources/js/editor.js', 'public/js')
-   .js('resources/js/app.js', 'public/js')
-   .js('resources/js/admin.js', 'public/js')
-   .js('resources/js/markdown.js', 'public/js')
+/* Allow multiple Laravel Mix applications*/
+require("laravel-mix-merge-manifest");
+mix.mergeManifest();
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            vars: path.resolve("resources/sass/theme/_vars.scss")
+        }
+    }
+})
+    .js("resources/js/editor.js", "public/js")
+    .js("resources/js/app.js", "public/js")
+    .js("resources/js/admin.js", "public/js")
+    .js("resources/js/markdown.js", "public/js");
 
 if (!mix.inProduction()) {
-   mix
-      .sass('resources/sass/dev.scss', 'public/css/app.css')
-      .sass('resources/sass/theme.scss', 'public/css')
-      .disableNotifications()
-      .options({ processCssUrls: false })
-      .browserSync({ proxy: 'http://127.0.0.1:8000' });
+    mix.sass("resources/sass/dev.scss", "public/css/app.css")
+        .sass("resources/sass/theme.scss", "public/css")
+        .disableNotifications()
+        .options({
+            processCssUrls: false
+        })
+        .browserSync({
+            proxy: "http://127.0.0.1:8000"
+        });
 } else {
-   mix
-      .sass('resources/sass/app.scss', 'public/css')
-      .disableNotifications()
-      .version()
+    mix.sass("resources/sass/app.scss", "public/css")
+        .disableNotifications()
+        .version();
 }
