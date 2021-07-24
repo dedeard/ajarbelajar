9b45216337c6aa5670a687b104b8d5ce49977281
dede ariansya <dedeariansya1@gmail.com>
2021-07-25 00:34:40 +0800

    #auth #adminlte

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,14 @@
-const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+const mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass('resources/sass/app.scss', 'public/css')
+
+mix.disableNotifications()
+
+if (!mix.inProduction()) {
+  mix.browserSync({
+    proxy: 'http://admin.ab.com',
+  })
+} else {
+  mix.version()
+}
