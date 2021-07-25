b7c50d481c93edbdb193ec46e0bd868a8c9c78e9
dede ariansya <dedeariansya1@gmail.com>
2021-07-26 03:27:59 +0800

    #joinminitutor

--- routes/api/app.php
+++ routes/api/app.php
@@ -1,1 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('join-minitutor', 'JoinMinitutorController@show');
+Route::get('join-minitutor/status', 'JoinMinitutorController@status');
+Route::post('join-minitutor', 'JoinMinitutorController@store');
