17f5c55c87c3b51b1f76c00d03cb4ffa3fb344dd
dede ariansya <dedeariansya1@gmail.com>
2021-07-26 23:40:12 +0800

    #request_minitutor

--- resources/views/minitutors/requests.blade.php
+++ resources/views/minitutors/requests.blade.php
@@ -9,36 +9,34 @@
           <a href="{{ route('minitutors.index') }}" class="btn btn-sm btn-primary">Kembali</a>
         </div>
       </div>
-      <div class="table-responsive">
-        <table class="table table-hover m-0">
-          <thead>
+      <table class="table table-hover m-0">
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Nama</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th class="text-center" style="width: 120px;">Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($requests as $data)
             <tr>
-              <th>Avatar</th>
-              <th>Nama</th>
-              <th>Username</th>
-              <th>Email</th>
-              <th class="text-center" style="width: 120px;">Aksi</th>
+              <td class="align-middle">
+                <span class="avatar">
+                  <img width="30px" src="{{ $data->user->avatar_url }}" />
+                </span>
+              </td>
+              <td class="align-middle"><a href="{{ route('users.show', $data->user->id) }}">{{ $data->user->name }}</a></td>
+              <td class="align-middle">{{ $data->user->username }}</td>
+              <td class="align-middle">{{ $data->user->email }}</td>
+              <td class="text-center p-0 align-middle">
+                <a href="{{ route('minitutors.request.show', $data->id) }}" class="btn btn-default btn-sm"><i class="fas fa-eye"></i></a>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            @foreach ($requests as $data)
-              <tr>
-                <td class="align-middle">
-                  <span class="avatar">
-                    <img src="{{ $data->user->avatar_url }}" />
-                  </span>
-                </td>
-                <td class="align-middle"><a href="{{ route('users.show', $data->user->id) }}">{{ $data->user->name }}</a></td>
-                <td class="align-middle">{{ $data->user->username }}</td>
-                <td class="align-middle">{{ $data->user->email }}</td>
-                <td class="text-center p-0 align-middle">
-                  <a href="{{ route('minitutors.request.show', $data->id) }}" class="btn btn-default btn-sm"><i class="fas fa-eye"></i></a>
-                </td>
-              </tr>
-            @endforeach
-          </tbody>
-        </table>
-      </div>
+          @endforeach
+        </tbody>
+      </table>
       <div class="card-footer">
         {{ $requests->links() }}
       </div>
