bfc7d01f271685419bd2fe08a9f055e8171d1eba
Dede ariansya <me@dedeard.my.id>
2023-03-05 10:32:22 +0800

    Remove `Show password` toggle.

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -8,24 +8,18 @@
 ])
 
 @php
-$name = $model ?? $name;
-$error = $errors->first($name);
-if (!$value) {
-    $value = $type !== 'password' ? old($name) : '';
-}
-$inputClass = 'block w-full flex-1 border-gray-300 rounded shadow placeholder:text-gray-500';
-if ($error) {
-    $inputClass = $inputClass . ' border-red-600';
-}
-if ($type === 'password') {
-    $inputClass = $inputClass . ' pr-8';
-}
-if (!$value) {
-    $value = $type !== 'password' ? old($name) : '';
-}
+  $name = $model ?? $name;
+  $error = $errors->first($name);
+  if (!$value) {
+      $value = $type !== 'password' ? old($name) : '';
+  }
+  $inputClass = 'block w-full flex-1 border-gray-300 rounded shadow placeholder:text-gray-500';
+  if ($error) {
+      $inputClass = $inputClass . ' border-red-600';
+  }
 @endphp
 
-<div class="relative" x-data="{ showPassword: false }">
+<div class="relative">
   @if ($type === 'select')
     <select id="{{ $name }}" class="{{ $inputClass }}" name="{{ $name }}"
       @if ($model) wire:model="{{ $name }}" @endif {{ $attributes }}>
@@ -37,15 +31,7 @@ if (!$value) {
   @else
     <input id="{{ $name }}" class="{{ $inputClass }}" type="{{ $type }}" name="{{ $name }}"
       value="{{ $value }}" placeholder="{{ $placeholder }}"
-      x-bind:type="{{ $type === 'password' ? "showPassword ? 'text' : 'password'" : "'text'" }}"
       @if ($model) wire:model="{{ $name }}" @endif {{ $attributes }} />
-
-    @if ($type === 'password')
-      <button type="button" class="absolute top-0 right-0 flex h-10 w-10 cursor-pointer items-center justify-center" aria-hidden="true"
-        @click="showPassword = ! showPassword">
-        <i class="ft" x-bind:class="showPassword ? 'ft-eye-off' : 'ft-eye'"></i>
-      </button>
-    @endif
   @endif
   @if ($error)
     <span class="block text-xs text-red-900">{{ $error }}</span>
