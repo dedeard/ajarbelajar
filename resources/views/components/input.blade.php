b557b12e7e548c224074dfbc89eda8423ad0f3dc
Dede ariansya <me@dedeard.my.id>
2023-03-05 12:52:58 +0800

    Add `Show password` toggle.

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -17,9 +17,12 @@
   if ($error) {
       $inputClass = $inputClass . ' border-red-600';
   }
+  if ($type === 'password') {
+      $inputClass = $inputClass . ' pr-8';
+  }
 @endphp
 
-<div class="relative">
+<div class="relative" x-data="{ showPassword: false }">
   @if ($type === 'select')
     <select id="{{ $name }}" class="{{ $inputClass }}" name="{{ $name }}"
       @if ($model) wire:model="{{ $name }}" @endif {{ $attributes }}>
@@ -31,7 +34,15 @@
   @else
     <input id="{{ $name }}" class="{{ $inputClass }}" type="{{ $type }}" name="{{ $name }}"
       value="{{ $value }}" placeholder="{{ $placeholder }}"
+      x-bind:type="{{ $type === 'password' ? "showPassword ? 'text' : 'password'" : "'text'" }}"
       @if ($model) wire:model="{{ $name }}" @endif {{ $attributes }} />
+
+    @if ($type === 'password')
+      <button type="button" class="absolute top-0 right-0 flex h-10 w-10 cursor-pointer items-center justify-center" aria-hidden="true"
+        @click="showPassword = ! showPassword">
+        <i class="ft" x-bind:class="showPassword ? 'ft-eye-off' : 'ft-eye'"></i>
+      </button>
+    @endif
   @endif
   @if ($error)
     <span class="block text-xs text-red-900">{{ $error }}</span>
