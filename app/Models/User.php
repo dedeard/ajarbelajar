b974cc3f76c534bd14b6c3df30dc940a03a55e19
Dede ariansya <me@dedeard.my.id>
2023-03-14 19:31:22 +0800

    Remove article

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,11 +49,6 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(Lesson::class);
     }
 
-    public function articles(): HasMany
-    {
-        return $this->hasMany(Article::class);
-    }
-
     public function getAvatarUrlAttribute(): string
     {
         $url = 'https://www.gravatar.com/avatar/';
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -12,7 +12,3 @@ Route::resource('/lessons', 'LessonsController');
 Route::post('/lessons/{lesson}/cover', 'LessonsController@updateCover')->name('lessons.update.cover');
 Route::post('/lessons/{lesson}/episode', 'LessonsController@uploadEpisode')->name('lessons.store.episode');
 Route::put('/lessons/{lesson}/description', 'LessonsController@updateDescription')->name('lessons.update.description');
-
-Route::resource('/articles', 'ArticlesController');
-Route::post('/articles/{article}/cover', 'ArticlesController@updateCover')->name('articles.update.cover');
-Route::put('/articles/{article}/content', 'ArticlesController@updateContent')->name('articles.update.content');
