9640d2ef8d7b5d672e0b50af00036f440155b240
Dede ariansya <me@dedeard.my.id>
2023-03-16 22:08:42 +0800

    fix favicon

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -11,6 +11,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta name="turbo-cache-control" content="no-cache" />
+  <link rel="icon" type="image/x-icon" href="/favicon.ico">
   {{ $head }}
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
