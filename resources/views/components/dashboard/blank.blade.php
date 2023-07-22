313f366980de781ac516a0e23aa4098ec55420d9
Dede ariansya <dedeariansya1@gmail.com>
2023-07-22 17:04:49 +0800

    update `UI`

--- resources/views/components/dashboard/blank.blade.php
+++ resources/views/components/dashboard/blank.blade.php
@@ -1,5 +1,7 @@
 <div class="container flex flex-1 p-3">
-  <div class="bg-white border flex felx-col flex-1 p-5 justify-center items-center">
-    <p class="text-center text-xl py-5 md:text-3xl">{{ $slot }}</p>
+  <div class="flex flex-1 bg-white border p-3">
+    <div class="bg-white border flex felx-col flex-1 p-5 justify-center items-center">
+      <p class="text-center text-xl py-5 md:py-8 md:text-2xl">{{ $slot }}</p>
+    </div>
   </div>
 </div>
