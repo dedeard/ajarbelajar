fc7e36f94d8befc25ee5a00d31e1a885ec297b80
Dede ariansya <dedeariansya1@gmail.com>
2023-07-22 20:30:36 +0800

    create ActivityFactory.php and ActivitiesControllerTest.php

---
+++ database/factories/ActivityFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Episode;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Activity>
+ */
+class ActivityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'episode_id' => Episode::factory(),
+        ];
+    }
+}
---
+++ tests/Feature/Dashboard/ActivitiesControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Controllers\Dashboard;
+
+use App\Models\Activity;
+use App\Models\Episode;
+use App\Models\Lesson;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ActivitiesControllerTest extends TestCase
+{
+  use RefreshDatabase, WithFaker;
+
+  public function testIndex()
+  {
+    // Create a user and some activities with associated episodes and lessons
+    $user = User::factory()->create();
+    $lesson = Lesson::factory()->create();
+    $episode = Episode::factory()->create(['lesson_id' => $lesson->id]);
+    $activity = Activity::factory()->create(['user_id' => $user->id, 'episode_id' => $episode->id]);
+
+    // Make a GET request to the index endpoint
+    $response = $this->actingAs($user)->get('/dashboard/activities');
+
+    // Assert the response is successful and the view contains the activity data
+    $response->assertStatus(200);
+    $response->assertViewIs('dashboard.activities');
+    $response->assertViewHas('activities');
+    $response->assertSee($activity->episode->title);
+  }
+}
