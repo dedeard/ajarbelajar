69dba3282e3c154729fb3c7ab7b8b7124b29210c
Dede ariansya <dedeariansya1@gmail.com>
2023-07-22 10:57:22 +0800

    Show blank if no `comments`

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,5 +1,15 @@
-<ul>
-  @foreach ($comments as $comment)
+<div>
+  @if(!count($comments))
+  <div class="p-3">
+    <div class="border p-5 bg-gray-50">
+      <p class="text-center text-xl px-3 py-8 md:text-2xl font-light">BELUM ADA KOMENTAR</p>
+    </div>
+  </div>
+  @else
+  <ul>
+    @foreach ($comments as $comment)
     <livewire:comment-list :key="$comment->id" :comment="$comment" :user="$user" />
-  @endforeach
-</ul>
+    @endforeach
+  </ul>
+  @endif
+</div>
