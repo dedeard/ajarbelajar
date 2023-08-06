493543a93278bbae34c2ac754fc7c4db47d2cd89
Dede ariansya <dedeariansya1@gmail.com>
2023-08-06 19:29:28 +0800

    🐞 fix: fix on episode processed.

--- app/Jobs/EpisodeUpdatedJob.php
+++ app/Jobs/EpisodeUpdatedJob.php
@@ -2,25 +2,29 @@
 
 namespace App\Jobs;
 
-use App\Events\EpisodeUpdated;
 use App\Models\Episode;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class EpisodeUpdatedJob
+class EpisodeUpdatedJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(public int $episodeId, public array $data)
+    {
+    }
 
     /**
      * Execute the job.
      */
-    public function handle(EpisodeUpdated $event): void
+    public function handle(): void
     {
-        $episode = Episode::find($event->episodeId);
+        $episode = Episode::find($this->episodeId);
         if ($episode) {
-            $episode->update($event->data);
+            $episode->update($this->data);
         }
     }
 }
