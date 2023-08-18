75f1a9eba69d711078193e76b35433a0c8bec8b4
Dede ariansya <dedeariansya1@gmail.com>
2023-08-18 18:56:10 +0800

    🚅 perfs: import from namespace

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use DragonCode\Support\Facades\Filesystem\File;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,10 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \DB::listen(function ($query) {
-            \File::append(
+        DB::listen(function ($query) {
+            File::append(
                 storage_path('/logs/query.log'),
-                '['.date('Y-m-d H:i:s').']'.PHP_EOL.$query->sql.' ['.implode(', ', $query->bindings).']'.PHP_EOL.PHP_EOL
+                '[' . date('Y-m-d H:i:s') . ']' . PHP_EOL . $query->sql . ' [' . implode(', ', $query->bindings) . ']' . PHP_EOL . PHP_EOL
             );
         });
     }
