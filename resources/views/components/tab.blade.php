abcf88d2466c5b8baf8e3d0d5be2c1b5dea9a6f5
Dede Ariansya <me@dedeard.my.id>
2023-09-06 17:11:46 +0800

    🖥️ wip: allow two tabs.

--- resources/views/components/tab.blade.php
+++ resources/views/components/tab.blade.php
@@ -1,7 +1,7 @@
-@props(['route', 'tab', 'lists', 'default' => ''])
+@props(['route', 'tab', 'lists', 'default' => '', 'class' => 'grid-cols-3'])
 
 <div class="bg-gray-300">
-  <div class="grid grid-cols-3 gap-px">
+  <div class="{{ $class }} grid gap-px">
     @foreach ($lists as $key => $value)
       <a href="{{ route($route, $value['params']) }}"
         class="@if ($tab === $key) bg-gray-50 border-primary-600 @else hover:bg-gray-50 bg-white @endif block border-b pb-3 pt-4 text-center text-sm font-semibold uppercase leading-none tracking-wider">{{ $value['text'] }}</a>
