5041b88a786cd3c22137dace51c332c439ad07e9
Dede Ariansya <me@dedeard.my.id>
2023-10-01 01:17:10 +0800

    🐞 fix: border color.

--- resources/views/components/comments.blade.php
+++ resources/views/components/comments.blade.php
@@ -32,7 +32,7 @@
   @endauth
 
   <template x-if="$store.commentStore.comments.length == 0">
-    <div class="p-3">
+    <div class="border p-3">
       <div class="border bg-gray-50 p-5">
         <p class="px-3 py-8 text-center text-xl font-light md:text-2xl"
           x-text="$store.commentStore.loadLoading ? 'Sedang memuat komentar' : 'Belum ada komentar'">
